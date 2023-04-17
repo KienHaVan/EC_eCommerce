c60ad8e8ff183d769cc7450e46cbc758d6a0fa2f
KienHaVan <hakien351998@gmail.com>
2023-04-17 10:44:59 +0700

    add react router dom

---
+++ src/routes/MainRoute.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export const MainRoute = () => {
+  return <BrowserRouter>MainRoute</BrowserRouter>;
+};
